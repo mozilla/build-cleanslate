c6286f1c2a2b17c47ad9c204b8c3155178d8df06
Morgan Phillips <winter2718@gmail.com>
2014-11-23 02:28:05 +0000

    Add support for continuous integration.

---
+++ setup.py
@@ -0,0 +1,19 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from setuptools import setup
+
+setup(
+    name="cleanslate",
+    version="0.0.0",
+    description="Reset a users running processes to a previously recorded state.",
+    author="Morgan Phillips",
+    author_email="winter2718@gmail.com",
+    py_packages=[
+        "cleanslate",
+    ],
+    scripts=["cleanslate.py"],
+    url="https://github.com/mrrrgn/build-cleanslate",
+    setup_requires=["nose==1.3.4"],
+)
