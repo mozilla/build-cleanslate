c00bdc3a3741586cf52f1d7aac5a861f99649170
Morgan Phillips <winter2718@gmail.com>
2014-12-17 02:56:31 +0000

    Restructure the cleanslate project to allow for automatic adaptation across platforms
    Cleanslate must support system level operation across many platforms. As a result
    it is desirable to be able to define various "cleaner" types, then assign them to a
    platform, and have the utility expose any compatible cleaners at runtime.
    
    This makes adding new platform specific cleaners much simpler and mitigates any need
    to modify the actual cleanslate.py script in order for them to be surfaced. This
    architecture also opens the possibility of a plugin system.

--- setup.py
+++ setup.py
@@ -6,14 +6,17 @@ from setuptools import setup
 
 setup(
     name="cleanslate",
-    version="1.2",
+    version="1.3",
     description="Reset a users running processes to a previously recorded state.",
     author="Morgan Phillips",
     author_email="winter2718@gmail.com",
-    py_packages=[
-        "cleanslate",
+    packages=[
+        "platform_cleaners",
     ],
     scripts=["cleanslate.py"],
     url="https://github.com/mrrrgn/build-cleanslate",
-    setup_requires=["nose==1.3.4"],
+    setup_requires=[
+        "six",
+        "nose==1.3.4",
+    ],
 )
